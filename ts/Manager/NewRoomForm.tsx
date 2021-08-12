e92888b2b489de69bdd17e610c6687ab2516491e
Jonas <acc.jo@posteo.de>
2021-08-12 09:37:55 +0200

    test: remove default value

--- ts/Manager/NewRoomForm.tsx
+++ ts/Manager/NewRoomForm.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const NewRoomForm: React.FC<Props> = (props) => {
 	const [name, setName] = useState<string>('');
-	const [hidden, setHidden] = useState<boolean>('');
+	const [hidden, setHidden] = useState<boolean>();
 	const [processing, setProcessing] = useState<boolean>(false);
 	const [error, setError] = useState<string>('');
 	const [buttonName, setButtonName] = useState<string>(t('bbb', 'Show Outlook Add-In Rooms'));
